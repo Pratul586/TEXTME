212f1d2d5a4e8114ac7af4617d9b2adb6ccb8538
Pratul Singh <87355545+Pratul586@users.noreply.github.com>
2025-07-07 21:36:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,9 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+const __dirname1 = __dirname;
 
+  
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "../frontend/build")));
 
